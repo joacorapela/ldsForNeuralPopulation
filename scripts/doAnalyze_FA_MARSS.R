03570da96ce622b1cd6328ece77c95bc8f9da1fd
Joaquin Rapela <joacorapela@gmail.com>
2020-09-24 21:34:58 +0100

    Major changes (e.g., reading configuration from ini files)

--- scripts/doAnalyze_FA_MARSS.R
+++ scripts/doAnalyze_FA_MARSS.R
@@ -2,39 +2,48 @@
 require(MASS)
 require(MARSS)
 require(ini)
-# require(reshape2)
-source("../projectSrc/utils/kalmanFilter/buildGoNogoAndLaserInputs.R")
+source("../projectSrc/utils/kalmanFilter/buildGoNogoLaserAndInteractionsInputs.R")
 source("../projectSrc/utils/kalmanFilter/estimateKFInitialCondFA.R")
 source("../commonSrc/stats/kalmanFilter/fit_FA_MARSS.R")
 source("../commonSrc/stats/kalmanFilter/create_FA_MARSS.R")
 
 processAll <- function() {
-    region <- "v1"
-    shaft <- 1
-    obsDim <- 7
-    obsInputMemorySecs <- 0.0
-    stateDim <- rep(3, times=length(obsInputMemorySecs))
-    stateInputMemorySecs <- obsInputMemorySecs
-    analysisStart <- 180 #sec
-    analysisDur <- 60 # sec
-    maxIter <- 20
-    kfFunc <- "MARSSkfss"
-    stateOffsetType <- "unconstrained"
-    stateCovType <- "diagonal and unequal"
-    obsOffsetType <- "unconstrained"
-    obsCovType <- "diagonal and unequal"
-    initialStateMeanType <- "unconstrained"
-    initialStateCovType <- "diagonal and unequal"
-    dataFilename <- "results/task_2019-02-06_21-36-35_preprocessing_2019_04_05_15_04_39_ks2_timeSeries.RData"
-    resultsFilenamePattern <- "results/analysis_%s_shaft%d_obsDim%d_stateDim%02d_stateInputMemory%.02f_obsInputMemory%.02fstart%.02fsec_dur%.02fsec.RData"
+    estConfigNumber <- 1
+    estConfigFilenamePattern <- "data/%08d_estimation.ini"
+
+    estConfigFilename <- sprintf(estConfigFilenamePattern, estConfigNumber)
+    estConfig <- read.ini(estConfigFilename)
+
+    obsInputMemorySecs <- eval(parse(text=estConfig$inputs$obsInputMemorySecs))
+    stateInputMemorySecs <- eval(parse(text=estConfig$inputs$stateInputMemorySecs))
+
+    region <- estConfig$control_variables$region
+    shaft <- as.numeric(estConfig$control_variables$shaft)
+    stateDim <- eval(parse(text=estConfig$control_variables$stateDim))
+    analysisStartTimeSecs <- as.double(estConfig$control_variables$analysisStartTimeSecs)
+    analysisDurSecs <- as.double(estConfig$control_variables$analysisDurSecs)
+
+    maxIter <- as.numeric(estConfig$EM$maxIter)
+
+    kfFunc <- estConfig$MARSS$kfFunc
+    stateOffsetType <- estConfig$MARSS$stateOffsetType
+    stateCovType <- estConfig$MARSS$stateCovType
+    obsOffsetType <- estConfig$MARSS$obsOffsetType
+    obsCovType <- estConfig$MARSS$obsCovType
+    initialStateMeanType <- estConfig$MARSS$initialStateMeanType
+    initialStateCovType <- estConfig$MARSS$initialStateCovType
+
+    dataFilename <-  estConfig$filenames$dataFilename
+    estMetaDataFilenamePattern <- estConfig$filenames$estMetaDataFilenamePattern
+    estResFilenamePattern <- estConfig$filenames$estResFilenamePattern
 
     stopifnot(length(stateDim)==length(stateInputMemorySecs) & length(stateInputMemorySecs)==length(obsInputMemorySecs))
     #
     data <- get(load(dataFilename))
     sRate <- data$sRate
     binSizeSecs <- (data$breaks[2]-data$breaks[1])/sRate
-    analysisSamples <- (analysisStart*sRate)+(1:(analysisDur*sRate))
-    spikeCounts <- data[[sprintf("%sShaft%dSpikeCounts", region, shaft)]][1:obsDim,analysisSamples]
+    analysisSamples <- (analysisStartTimeSecs*sRate)+(1:(analysisDurSecs*sRate))
+    spikeCounts <- data[[sprintf("%sShaft%dSpikeCounts", region, shaft)]][,analysisSamples]
     goStim <- data$goStim[analysisSamples]
     nogoStim <- data$nogoStim[analysisSamples]
     laserStim <- data$laserStim[analysisSamples]
@@ -43,23 +52,40 @@ processAll <- function() {
 
     for(i in 1:length(stateDim)) {
         show(sprintf("Processing number of latents=%d, state input memory=%f sec, observation input memory=%f sec", stateDim[i], stateInputMemorySecs[i], obsInputMemorySecs[i]))
+
+        exit <- FALSE
+        while(!exit) {
+            estNumber <- sample(1e8, 1)
+            estMetaDataFilename <- sprintf(estMetaDataFilenamePattern, estNumber)
+            if(!file.exists(estMetaDataFilename)) {
+                exit <- TRUE
+            }
+        }
+
+        estMetaDataFilename <- sprintf(estMetaDataFilenamePattern, estNumber)
+        metaData <- list()
+        metaData[["estimation_config_info"]] <- list(estConfigNumber=estConfigNumber)
+        metaData[["model_info"]] <- list(stateDim=stateDim[i], stateInputMem=stateInputMemorySecs[i], obsInputMem=obsInputMemorySecs[i])
+        write.ini(x=metaData, filepath=estMetaDataFilename)
+        show(sprintf("Saved estimation meta data to: %s", estMetaDataFilename))
+
         if(!is.nan(stateInputMemorySecs[i])) {
-            stateInputs <- buildGoNogoAndLaserInputs(goStim=goStim, nogoStim=nogoStim, laserStim=laserStim, inputMemory=stateInputMemorySecs[i]*sRate)
+            stateInputs <- buildGoNogoLaserAndInteractionsInputs(goStim=goStim, nogoStim=nogoStim, laserStim=laserStim, inputMemory=stateInputMemorySecs[i]*sRate)
         } else {
             stateInputs <- NA
         }
         if(!is.nan(obsInputMemorySecs[i])) {
-            obsInputs <- buildGoNogoAndLaserInputs(goStim=goStim, nogoStim=nogoStim, laserStim=laserStim, inputMemory=obsInputMemorySecs[i]*sRate)
+            obsInputs <- buildGoNogoLaserAndInteractionsInputs(goStim=goStim, nogoStim=nogoStim, laserStim=laserStim, inputMemory=obsInputMemorySecs[i]*sRate)
         } else {
             obsInputs <- NA
         }
         kem <- fit_FA_MARSS(observations=tSpikeRates, stateDim=stateDim[i], stateInputs=stateInputs, stateOffsetType=stateOffsetType, stateCovType=stateCovType, obsInputs=obsInputs, obsOffsetType=obsOffsetType, obsCovType=obsCovType, initialStateMeanType=initialStateMeanType, initialStateCovType=initialStateCovType, maxIter=maxIter, kfFunc=kfFunc)
+        kem <- MARSSaic(kem)
         show(sprintf("number of latents=%d, state input memory=%f sec, observation input memory=%f sec: logLik=%f, AIC=%f, AICc=%f", stateDim[i], stateInputMemorySecs[i], obsInputMemorySecs[i], kem$logLik, kem$AIC, kem$AICc))
         #
-        resultsFilename <- sprintf(resultsFilenamePattern, stateDim[i], stateInputMemorySecs[i], obsInputMemorySecs[i])
-        results <- list(kem=kem, tSpikeRates=tSpikeRates, stateInputs=stateInputs, obsInputs=obsInputs, sRate=sRate, startTime=analysisStart)
-        save(results, file=resultsFilename)
-        return()
+        estResFilename <- sprintf(estResFilenamePattern, estNumber)
+        estRes <- list(kem=kem, stateDim=stateDim[i], stateInputMem=stateInputMemorySecs[i], obsInputMem=obsInputMemorySecs[i], tSpikeRates=tSpikeRates, stateInputs=stateInputs, obsInputs=obsInputs, sRate=sRate, startTime=analysisStartTimeSecs)
+        save(estRes, file=estResFilename)
     }
 }
 
