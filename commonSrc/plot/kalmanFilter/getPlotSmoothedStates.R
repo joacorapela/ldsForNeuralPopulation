eceef67d5d932e496d6513d2e04655b3947b1779
Joaquin Rapela <joacorapela@gmail.com>
2020-09-29 08:07:17 +0100

    Moved getStimRecs to its own file

--- commonSrc/plot/kalmanFilter/getPlotSmoothedStates.R
+++ commonSrc/plot/kalmanFilter/getPlotSmoothedStates.R
@@ -1,52 +1,4 @@
-getPlotSmoothedStates <- function(time, xtT, VtT, inputs=NA, statesToPlot=NA, xlab="Time (sec)", ylab="p(x|y_1,...,yN)", goStimColor="green", nogoStimColor="red", laserStimColor="blue", stimOpacity=0.2) {
-    # inputs[1,]: go
-    # inputs[2,]: nogo
-    # inputs[3,]: laser
-
-    getRecs <- function(onsets, durations, color, opacity, ymin, ymax) {
-        recs <- vector(mode="list", length=length(onsets))
-        for(i in 1:length(onsets)) {
-            x0 <- onsets[i]
-            x1 <- onsets[i]+durations[i]
-            recs[[i]] <- list(type="rect",
-                              fillcolor=color,
-                              line=list(color=color),
-                              opacity=opacity,
-                              x0=x0, x1=x1, xref="x",
-                              y0=ymin, y1=ymax, yref="y")
-        }
-        return(recs)
-    }
-
-    getStimRecs <- function(inputs, ymin, ymax) {
-        goDiff <- diff(inputs[1,])
-        goOnIndices <- which(goDiff>0)+1
-        goOffIndices <- which(goDiff<0)+1
-        goOnIndices <- goOnIndices[1:min(length(goOnIndices), length(goOffIndices))]
-        goOffIndices <- goOffIndices[1:min(length(goOnIndices), length(goOffIndices))]
-
-        goRecs <- getRecs(onsets=time[goOnIndices], durations=time[goOffIndices]-time[goOnIndices], color=goStimColor, opacity=stimOpacity, ymin=ymin, ymax=ymax)
-
-        nogoDiff <- diff(inputs[2,])
-        nogoOnIndices <- which(nogoDiff>0)+1
-        nogoOffIndices <- which(nogoDiff<0)+1
-        nogoOnIndices <- nogoOnIndices[1:min(length(nogoOnIndices), length(nogoOffIndices))]
-        nogoOffIndices <- nogoOffIndices[1:min(length(nogoOnIndices), length(nogoOffIndices))]
-
-        nogoRecs <- getRecs(onsets=time[nogoOnIndices], durations=time[nogoOffIndices]-time[nogoOnIndices], color=nogoStimColor, opacity=stimOpacity, ymin=ymin, ymax=ymax)
-
-        laserDiff <- diff(inputs[3,])
-        laserOnIndices <- which(laserDiff>0)+1
-        laserOffIndices <- which(laserDiff<0)+1
-        laserOnIndices <- laserOnIndices[1:min(length(laserOnIndices), length(laserOffIndices))]
-        laserOffIndices <- laserOffIndices[1:min(length(laserOnIndices), length(laserOffIndices))]
-
-        laserRecs <- getRecs(onsets=time[laserOnIndices], durations=time[laserOffIndices]-time[laserOnIndices], color=laserStimColor, opacity=stimOpacity, ymin=ymin, ymax=ymax)
-
-        stimRecs <- c(goRecs, nogoRecs, laserRecs)
-
-        return(stimRecs)
-    }
+getPlotSmoothedStates <- function(time, xtT, VtT, stateInputMemorySamples, inputs=NA, statesToPlot=NA, xlab="Time (sec)", ylab="p(x|y_1,...,yN)", goStimColor="green", nogoStimColor="red", laserStimColor="blue", stimOpacity=0.2) {
 
     M <- nrow(xtT)
     N <- ncol(xtT)
@@ -78,13 +30,14 @@ getPlotSmoothedStates <- function(time, xtT, VtT, inputs=NA, statesToPlot=NA, xl
         cbLower <- mean-1.96*std
         ymax <- max(ymax, max(cbUpper))
         ymin <- min(ymin, min(cbLower))
-        fig <- fig%>%add_trace(x=time, y=mean, mode="lines", name=sprintf("state%d", i), line=list(color=sprintf("rgba(%d,%d,%d,%f)", rgbValues[1,1], rgbValues[2,1], rgbValues[3,1], 1), dash="solid"))
-        fig <- fig%>%add_trace(x=time, y=cbUpper, mode="lines", line=list(color="rgba(0,0,0,0)"), name=sprintf("forecast[,%d]", i), showlegend=FALSE)
-        fig <- fig%>%add_trace(x=time, y=cbLower, mode="lines", line=list(color="rgba(0,0,0,0)"), name=sprintf("forecast[,%d]", i), showlegend=FALSE, fill="tonexty", fillcolor=sprintf("rgba(%d,%d,%d,%f)", rgbValues[1,1], rgbValues[2,1], rgbValues[3,1], 0.2))
+        rgbaColorName <- sprintf("rgba(%d,%d,%d,%f)", rgbValues[1,1], rgbValues[2,1], rgbValues[3,1], 1)
+        fig <- fig%>%add_trace(x=time, y=mean, mode="lines+markers", name=sprintf("state%d", i), line=list(color=rgbaColorName, dash="solid"), marker=list(color=rgbaColorName, symbol="asterisk-open"))
+        fig <- fig%>%add_trace(x=time, y=cbUpper, mode="lines", line=list(color="rgba(0,0,0,0)"), name=sprintf("state[,%d]", i), showlegend=FALSE)
+        fig <- fig%>%add_trace(x=time, y=cbLower, mode="lines", line=list(color="rgba(0,0,0,0)"), name=sprintf("state[,%d]", i), showlegend=FALSE, fill="tonexty", fillcolor=sprintf("rgba(%d,%d,%d,%f)", rgbValues[1,1], rgbValues[2,1], rgbValues[3,1], 0.2))
     }
     fig <- fig%>%layout(xaxis=list(title=xlab), yaxis=list(title=ylab))
     if(!is.na(inputs[1])) {
-        stimRecs <- getStimRecs(inputs=inputs, ymin=ymin, ymax=ymax)
+        stimRecs <- getStimRecs(time=time, inputs=inputs, inputMemorySamples=stateInputMemorySamples, ymin=ymin, ymax=ymax, goStimColor=goStimColor, nogoStimColor=nogoStimColor, laserStimColor=laserStimColor, stimOpacity=stimOpacity)
         fig <- fig%>%layout(shapes=stimRecs)
     }
 #     if(!is.na(inputs[1])) {
