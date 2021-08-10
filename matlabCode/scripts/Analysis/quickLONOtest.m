c29ea949d20eece5aa385c8e6e2f67aac8037cf3
Mitra Javadzadeh <mitra.javadzadehno@unibas.ch>
2021-08-10 11:23:36 +0100

    adding eRFs to compare LONO and full model

--- matlabCode/scripts/Analysis/quickLONOtest.m
+++ matlabCode/scripts/Analysis/quickLONOtest.m
@@ -2,8 +2,8 @@
 
 % fit on training dataset
 LONOpath = '/mnt/data/Mitra/cache/repos/ldsForNeuralPopulation/results/VL61/trial_based_LONO/50msBins';
-LONOname_V1 = 'Fold1_V1_PLDSfitRes_21_08_04_14_43_12.mat';
-LONOname_LM = 'Fold1_LM_PLDSfitRes_21_08_04_15_39_35.mat';
+LONOname_V1 = 'Fold1_V1_PLDSfitRes_21_08_09_14_27_24.mat';
+LONOname_LM = 'Fold1_LM_PLDSfitRes_21_08_09_15_19_33.mat';
 
 % fit on fulldataset
 Fullpath = '/mnt/data/Mitra/cache/repos/ldsForNeuralPopulation/results/VL61/trial_based/50msBins';
@@ -18,9 +18,9 @@ subplot(1,2,2);imagesc(resFull.params.model.A);
 
 figure;subplot(1,2,1);imagesc(resLONO.params.model.B);
 subplot(1,2,2);imagesc(resFull.params.model.B);
-% strange B behavior
+
 figure;plot(cell2mat(arrayfun(@(x) x.u(1,:) - x.u(2,:), resLONO.seq,'UniformOutput',0)')')
-% but it is not due to u being the same for go and nogo
+
 % checking trial averaged state variables:
 for state=1:9
     figure
@@ -30,3 +30,30 @@ end
 
 figure;subplot(1,2,1);imagesc(resLONO.params.model.C);
 subplot(1,2,2);imagesc(resFull.params.model.C);
+%% receptive field of neurons calculated from the full or LONO model: very similar
+
+n=11;   %neuron number 
+RF = [];
+
+for in = 1:4
+    for i=1:10
+        RF{n}(i,in) = resFull.params.model.C(n,:)*((resFull.params.model.A)^(i-1))*resFull.params.model.B(:,in);
+    end
+end
+
+
+figure;plot(RF{n}(:,1),'g');hold on;plot(RF{n}(:,2),'r');
+hold on;plot(RF{n}(:,3),'g');hold on;plot(RF{n}(:,4),'r');
+
+RF = [];
+
+for in = 1:4
+    for i=1:10
+        RF{n}(i,in) = resLONO.params.model.C(n,:)*((resLONO.params.model.A)^(i-1))*resLONO.params.model.B(:,in);
+    end
+end
+
+
+
+hold on;plot(RF{n}(:,1),'g--');hold on;plot(RF{n}(:,2),'r--');
+hold on;plot(RF{n}(:,3),'g--');hold on;plot(RF{n}(:,4),'r--');
