92a904b566353a32a86d671edb6d36f0fa3f815a
Mitra Javadzadeh <mitra.javadzadehno@unibas.ch>
2021-09-27 14:56:04 +0100

    plot details, default params

--- matlabCode/scripts/Analysis/InputAndDynamicsAlignment_combinedAnimals.m
+++ matlabCode/scripts/Analysis/InputAndDynamicsAlignment_combinedAnimals.m
@@ -14,7 +14,9 @@ real_D = cell(1,7);
 for animalnum = 1:7
     
     Mfile = dir(['/mnt/data/Mitra/cache/repos/ldsForNeuralPopulation/results/',animallist{animalnum},...
-        '/trial_based_MS_split_1/17msBins/*_',area,'_*.mat']);
+        '/trial_based_MS_split_1/17msBins/*_',area,'_nState8*.mat']);
+%         Mfile = dir(['/mnt/data/Mitra/cache/repos/ldsForNeuralPopulation/results/',animallist{animalnum},...
+%         '/trial_based_MS_split_1/17msBins/*_',area,'_*.mat']);
     cd(Mfile(end).folder)
     res = load(Mfile(end).name);
 
@@ -30,47 +32,95 @@ for animalnum = 1:7
     alpha{animalnum} = abs(alpha{animalnum}); % real or abs
     
 end
-
+%% intermediate plots
+figure
+for animalnum = 1:7
+    [~,ord]=sort(real_D{animalnum});
+ hold on;plot(real_D{animalnum}(ord),'k')
+ xlabel('state number')
+ ylabel('abs(lambda)')
+end
 %% plots
-gol = zeros(8,10);
-nogol = zeros(8,10);
-gov = zeros(1,10);
-nogov = zeros(1,10);
+nstates = 8;
+
+gol = zeros(8,nstates);
+nogol = zeros(8,nstates);
+gov = zeros(1,nstates);
+nogov = zeros(1,nstates);
 figure;
-for animalnum = 3%:7
+for animalnum = 1:7
     [~,ord]=sort(real_D{animalnum});
     
-    subplot(2,9,9)
+    s = subplot(2,9,9);
     plot(real_D{animalnum}(ord),'k'); 
-    hold on;plot(1*(alpha{animalnum}(1,ord)),'Color',[0 1 0],'LineWidth',.01)
-    ylim([-1 1])
-    xlabel('state number');ylabel('visual input');
+    hold on;plot(1*(alpha{animalnum}(1,ord)),'Color',[0.8 0.8 0.8],'LineWidth',.01)
+    ylim([0 1])
+    xlabel('state number');
+    s.YTick = [0 1];
+    s.YTickLabel = [0 1];
+    s.YTick = [1 8];
+    s.YTickLabel = [ 1 8];   
+    ylabel('abs(alpha)');    
+    s.Title.String = ('visual input');
+    s.Title.FontWeight = 'normal';
+    s.Title.Position(2) = s.Title.Position(2)+0.005;
     
-    subplot(2,9,18)
+    s=subplot(2,9,18);
     plot(real_D{animalnum}(ord),'k'); 
-    hold on;plot(1*(alpha{animalnum}(2,ord)),'Color',[1 0 0],'LineWidth',.01)
-    ylim([-1 1])
-    xlabel('state number');ylabel('visual input');
+    hold on;plot(1*(alpha{animalnum}(2,ord)),'Color',[.8 0.8 0.8],'LineWidth',.01)
+    ylim([0 1])
+    xlabel('state number');
+    s.YTick = [0 1];
+    s.YTickLabel = [0 1];
+    s.YTick = [1 8];
+    s.YTickLabel = [ 1 8];   
+    ylabel('abs(alpha)');    
+    s.Title.String = ('visual input');
+    s.Title.FontWeight = 'normal';
+    s.Title.Position(2) = s.Title.Position(2)+0.005;
     
     gov = gov + alpha{animalnum}(1,ord);
     nogov = nogov + alpha{animalnum}(2,ord);
     for i =3:10
-        subplot(2,9,i-2)
+        s = subplot(2,9,i-2);
         plot(real_D{animalnum}(ord),'k');
-        hold on;plot(1*(alpha{animalnum}(i,ord)),'Color',[0 1 0],'LineWidth',.01)
-        ylim([-1 1])
+        hold on;plot(1*(alpha{animalnum}(i,ord)),'Color',[0.8 .8 0.8],'LineWidth',.01)
+        ylim([0 1])
         gol(i-2,:) = gol(i-2,:) + alpha{animalnum}(i,ord);
-        xlabel('state number');%ylabel('feedback input');       
+        xlabel('state number');%ylabel('feedback input');   
+        
+        s.YTick = [0 1];
+        s.YTickLabel = [0 1];
+        s.YTick = [1 8];
+        s.YTickLabel = [ 1 8];
+        if i ==3
+        ylabel('abs(alpha)');
+        end
+        s.Title.String = (['delay',num2str(i-2)]);
+        s.Title.FontWeight = 'normal';
+        s.Title.Position(2) = s.Title.Position(2)+0.01;
     end
     for i =11:18
-        subplot(2,9,i-1)
+        s= subplot(2,9,i-1);
         plot(real_D{animalnum}(ord),'k');
-        hold on;plot(1*(alpha{animalnum}(i,ord)),'Color',[1 0 0],'LineWidth',.01)
-        ylim([-1 1])
+        hold on;plot(1*(alpha{animalnum}(i,ord)),'Color',[.8 0.8 0.8],'LineWidth',.01)
+        ylim([0 1])
         nogol(i-10,:) = nogol(i-10,:) + alpha{animalnum}(i,ord);
         xlabel('state number');%ylabel('feedback input');
-    end
-end
+        
+        s.YTick = [0 1];
+        s.YTickLabel = [0 1];
+        s.YTick = [1 8];
+        s.YTickLabel = [ 1 8];
+        if i == 11
+        ylabel('abs(alpha)');
+        end
+        s.Title.String = (['delay',num2str(i-10)]);
+        s.Title.FontWeight = 'normal';
+        s.Title.Position(2) = s.Title.Position(2)+0.01;
+
+     end
+ end
 gol = gol/7;
 nogol = nogol/7;
 gov = gov/7;
